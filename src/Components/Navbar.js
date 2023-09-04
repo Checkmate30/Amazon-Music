4ec9c110d58a4ce39f614b44ce860fb009801639
Checkmate30 <vveksawant@gmail.com>
2023-09-04 23:22:48 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Navbar() {
+
+  return (
+    <div>
+        <img src="amazon-music\public\images\logo.png" alt="logo" />
+
+
+    </div>
+  )
+}
+
+export default Navbar
